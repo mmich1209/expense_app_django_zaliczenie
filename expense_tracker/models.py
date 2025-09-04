bb152ce361c4aa2dfc5312af3209bd7abdfe2584
Admin <mmichalski1209@gmail.com>
2025-09-04 15:09:17 +0200

    Added Expense model

--- expense_tracker/models.py
+++ expense_tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#model for saving up the expenses
+
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.PositiveIntegerField()
+    category = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name

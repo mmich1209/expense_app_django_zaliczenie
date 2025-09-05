265f325eac14f04a9ea2c00729b6e30b5f2276e3
Admin <mmichalski1209@gmail.com>
2025-09-05 14:38:43 +0200

    Created forms.py file. Inserted the form in the index.html.

--- expense_tracker/views.py
+++ expense_tracker/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Expense
+from .forms import ExpenseForm
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'expense_tracker/index.html')
+    expense_form = ExpenseForm()
+    return render(request, 'expense_tracker/index.html', {'expense_form': expense_form})
+

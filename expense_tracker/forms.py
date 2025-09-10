6db300c3a549a8d2e686ab245de72a2a15e923eb
Admin <mmichalski1209@gmail.com>
2025-09-10 11:42:23 +0200

    Improved the style of the app. Changed the language to polish on edit.html page.

--- expense_tracker/forms.py
+++ expense_tracker/forms.py
@@ -5,3 +5,7 @@ class ExpenseForm(ModelForm):
     class Meta:
         model = Expense
         fields = ('name', 'amount', 'category')
+
+    def clean_category(self):
+        category = self.cleaned_data['category']
+        return category.strip().capitalize()
